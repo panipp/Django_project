6531535df0acda8c7107fe05932f8f32aea7d5be
natchakorn19156 <33547485+natchakorn19156@users.noreply.github.com>
2019-05-01 20:28:59 +0700

    Update models.py

--- board/myapp/models.py
+++ board/myapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_id = models.CharField(max_length =100)
+    firstname = models.CharField(max_length = 50)
+    lastname = models.CharField(max_length = 50)
+    role = models.CharField(max_length = 1)
+
+class News(models.Model):
+    news_id = models.CharField(max_length = 100)
+    title = models.CharField(max_length=500)
+    date = models.DateField()
+    file = models.FileField(upload_to='files/',blank=False)
+    detail = models.TextField(max_length = 500 , blank = True, null = True)
+    def __str__(self):
+        return self.title, self.date
+
+class Board(models.Model):
+    board_id = models.CharField(max_length = 100)
+    detail = models.TextField(max_length = 100) 
+    image = models.ImageField() 
+    date = models.DateField()
+
+class Exam (models.Model):
+    exam_id = models.CharField(max_length = 100)
+    detail = models.CharField(max_length=500)
+    date = models.DateField()
+    category = models.CharField(max_length=50)
